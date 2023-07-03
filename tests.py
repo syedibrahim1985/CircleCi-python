6d240448b822c80ffa7069f11b6687e2c3606a47
Syed.Ibrahim <syed.ibrahim@invisinet.com>
2023-07-03 17:08:14 +0530

    add some changes

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name = "Syed"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "Syed")
+        self.assertEqual(upper_name, "SYED")
 
 
 if __name__ == '__main__':
